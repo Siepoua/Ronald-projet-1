a77466a5b66b8b7b1a35d97a05c590c27bb6f59b
Christ2009 <christstars2009@gmail.com>
2024-08-11 13:28:34 +0100

    Update loadconfig.js

--- scripts/cmds/loadconfig.js
+++ scripts/cmds/loadconfig.js
@@ -21,7 +21,7 @@ module.exports = {
 			success: "Config đã được load lại thành công"
 		},
 		en: {
-			success: "Config has been reloaded successfully"
+			success: "𝙇𝙖 𝙘𝙤𝙣𝙛𝙞𝙜𝙪𝙧𝙖𝙩𝙞𝙤𝙣 𝙖 é𝙩é 𝙧𝙚𝙘𝙝𝙖𝙧𝙜é𝙚 𝙘𝙝𝙚𝙯 𝘾𝙝𝙧𝙞𝙨𝙩𝙚𝙡𝙡𝙚 👻 𝙖𝙫𝙚𝙘 𝙨𝙪𝙘𝙘è𝙨 👻"
 		}
 	},
 
