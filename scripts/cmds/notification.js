7f7133ebe4a44bc31ff7e6f55c4dec3a1dd737ac
Christ2009 <christstars2009@gmail.com>
2024-08-11 13:38:48 +0100

    Update notification.js

--- scripts/cmds/notification.js
+++ scripts/cmds/notification.js
@@ -30,11 +30,11 @@ module.exports = {
 			errorSendingNotification: "Có lỗi xảy ra khi gửi đến %1 nhóm:\n%2"
 		},
 		en: {
-			missingMessage: "Please enter the message you want to send to all groups",
-			notification: "Notification from admin bot to all chat groups (do not reply to this message)",
-			sendingNotification: "Start sending notification from admin bot to %1 chat groups",
-			sentNotification: "✅ Sent notification to %1 groups successfully",
-			errorSendingNotification: "An error occurred while sending to %1 groups:\n%2"
+			missingMessage: "👻 𝙑𝙚𝙪𝙞𝙡𝙡𝙚𝙯 𝙨𝙖𝙞𝙨𝙞𝙧 𝙡𝙚 𝙢𝙚𝙨𝙨𝙖𝙜𝙚 𝙦𝙪𝙚 𝙫𝙤𝙪𝙨 𝙨𝙤𝙪𝙝𝙖𝙞𝙩𝙚𝙯 𝙚𝙣𝙫𝙤𝙮𝙚𝙧 à 𝙩𝙤𝙪𝙨 𝙡𝙚𝙨 𝙜𝙧𝙤𝙪𝙥𝙚𝙨 𝘾𝙝𝙧𝙞𝙨𝙩𝙚𝙡𝙡𝙚 👻 𝙫𝙖𝙨 𝙡𝙚 𝙛𝙖𝙞𝙧𝙚",
+			notification: "𝙉𝙤𝙩𝙞𝙛𝙞𝙘𝙖𝙩𝙞𝙤𝙣 𝙙𝙚 𝘾𝙝𝙧𝙞𝙨𝙩𝙚𝙡𝙡𝙚 👻 𝙖𝙙𝙢𝙞𝙣𝙞𝙨𝙩𝙧𝙖𝙩𝙚𝙪𝙧 𝙘𝙝𝙧𝙞𝙨_𝙨𝙩𝙖𝙧𝙩 à 𝙩𝙤𝙪𝙨 𝙡𝙚𝙨 𝙜𝙧𝙤𝙪𝙥𝙚𝙨 𝙙𝙚 𝙙𝙞𝙨𝙘𝙪𝙨𝙨𝙞𝙤𝙣 (𝙣𝙚 𝙧é𝙥𝙤𝙣𝙙𝙚𝙯 𝙥𝙖𝙨 à 𝙘𝙚 𝙢𝙚𝙨𝙨𝙖𝙜𝙚) 👻",
+			sendingNotification: "𝘾𝙤𝙢𝙢𝙚𝙣𝙘𝙚𝙧 à 𝙚𝙣𝙫𝙤𝙮𝙚𝙧 𝙙𝙚𝙨 𝙣𝙤𝙩𝙞𝙛𝙞𝙘𝙖𝙩𝙞𝙤𝙣𝙨 𝙙𝙚 𝘾𝙝𝙧𝙞𝙨𝙩𝙚𝙡𝙡𝙚 👻 𝙖𝙙𝙢𝙞𝙣𝙞𝙨𝙩𝙧𝙖𝙩𝙚𝙪𝙧 𝘾𝙝𝙧𝙞𝙨 𝙨𝙩𝙖𝙧𝙩 𝙖𝙪𝙭 𝙜𝙧𝙤𝙪𝙥𝙚𝙨 𝙙𝙚 𝙙𝙞𝙨𝙘𝙪𝙨𝙨𝙞𝙤𝙣 %1 👻",
+			sentNotification: "✅ 𝙇𝙖 𝙣𝙤𝙩𝙞𝙛𝙞𝙘𝙖𝙩𝙞𝙤𝙣 𝙙𝙚 𝘾𝙝𝙧𝙞𝙨𝙩𝙚𝙡𝙡𝙚 👻 𝙖 é𝙩é 𝙚𝙣𝙫𝙤𝙮é𝙚 𝙖𝙫𝙚𝙘 𝙨𝙪𝙘𝙘è𝙨 à %1 𝙜𝙧𝙤𝙪𝙥𝙚𝙨 👻",
+			errorSendingNotification: "𝙐𝙣𝙚 𝙚𝙧𝙧𝙚𝙪𝙧 𝙨'𝙚𝙨𝙩 𝙥𝙧𝙤𝙙𝙪𝙞𝙩𝙚 𝙘𝙝𝙚𝙯 𝘾𝙝𝙧𝙞𝙨𝙩𝙚𝙡𝙡𝙚 𝙡𝙚 𝙛𝙖𝙣𝙩ô𝙢𝙚 👻 𝙡𝙤𝙧𝙨 𝙙𝙚 𝙡'𝙚𝙣𝙫𝙤𝙞 à %1 𝙜𝙧𝙤𝙪𝙥𝙚𝙨 👻:\n%2"
 		}
 	},
 
