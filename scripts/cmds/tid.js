32aed8c1c7cebc1464cf77b20cc3e26a0c8a4ce6
Christ2009 <christstars2009@gmail.com>
2024-08-15 10:30:33 +0100

    Update tid.js

--- scripts/cmds/tid.js
+++ scripts/cmds/tid.js
@@ -7,7 +7,7 @@ module.exports = {
 		role: 0,
 		description: {
 			vi: "Xem id nhóm chat của bạn",
-			en: "View threadID of your group chat"
+			en: "𝘼𝙛𝙛𝙞𝙘𝙝𝙚𝙧 𝙡'𝙄𝘿 𝙙𝙪 𝙛𝙞𝙡 𝙙𝙚 𝙙𝙞𝙨𝙘𝙪𝙨𝙨𝙞𝙤𝙣 𝙙𝙚 𝙫𝙤𝙩𝙧𝙚 𝙜𝙧𝙤𝙪𝙥𝙚 𝙙𝙚 𝙙𝙞𝙨𝙘𝙪𝙨𝙨𝙞𝙤𝙣 👻"
 		},
 		category: "info",
 		guide: {
